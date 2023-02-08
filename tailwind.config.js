1caafd692fe07a355e410957fe47ae5444928213
Prerana Rao <mpreranarao@gmail.com>
2023-02-08 15:34:45 +0530

    Adding custom configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        black: '#000000',
+        richBlack: '#0C1821',
+        oxfordBlue: '#1B2A41',
+        charcoal: '#324A5F'
+      }
+    },
   },
   plugins: [],
 }
